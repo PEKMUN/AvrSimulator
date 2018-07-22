388bdbc79c72e14f395fd83d204e45a634040d03
PEKMUN <pekmun98@gmail.com>
2018-07-22 18:23:24 +0800

    Need to modify test

--- src/Simulator.c
+++ src/Simulator.c
@@ -14,8 +14,20 @@ AvrOperator AvrOperatorTable[256] = {
 //AVR Register
 uint8_t r[32] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
+/**
+ * Instruction:
+ * 		ADD Rd,Rr
+ *		0000 11rd dddd rrrr
+ * where
+ *		0 <= ddddd <= 31
+ *		0 <= rrrrr <= 31
+ */
 int add(uint8_t *codePtr)
 {
+  uint8_t rd, rr;
+  
+  //rd = ((*codePtr[1] & 0x1) << 7) | (*codePtr[0] & 0xf0);
+  //rr = ((*codePtr[1] & 0x2) << 3) | (*codePtr[0] & 0xf);
   return 0;
 }
 
@@ -34,7 +46,31 @@ int andi(uint8_t *codePtr)
   return 0;
 }
 
+/**
+ * Instruction:
+ * 		ADIW Rd + 1:Rd, K
+ *		1001 0110 KKdd KKKK
+ * where
+ *		0 <= KKKK <= 63
+ * 		dd is {
+ *			00 => 24,
+ *			01 => 26, 
+ *			10 => 28, 
+ *			11 => 30
+ *		}
+ */
 int adiw(uint8_t *codePtr)
 {
+  uint8_t rd, k;
+  uint16_t word;
+  
+  k = (*codePtr & 0xf) | ((*codePtr & 0xc0) >> 2);
+  rd = ((*codePtr & 0x30) >> 3) + 24;
+  
+  word = (((uint8_t)r[rd+2]) << 8) | r[rd];
+  word += k;
+
+  r[rd+1] = word >> 8;
+  r[rd] = word & 0xf;
   return 0;
 }
--- test/test_Simulator.c
+++ test/test_Simulator.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "unity.h"
 #include "Simulator.h"
 
@@ -5,7 +6,56 @@ void setUp(void){}
 
 void tearDown(void){}
 
-void test_Simulator_NeedToImplement(void)
+/**
+ * Instruction:
+ * 		ADIW Rd + 1:Rd, K
+ *		1001 0110 KKdd KKKK
+ * where
+ *		0 <= KKKK <= 63
+ * 		dd is {
+ *			00 => 24,
+ *			01 => 26, 
+ *			10 => 28, 
+ *			11 => 30
+ *		}
+ * 
+ * Simulate adiw R27,26, 13
+ * 		KKKKKK = 13 = 0xd = b'001101'
+ *		    dd = b'01' ==> 26
+ *		1001 0110 0001 1101
+ *		  9    6    1    d
+ */
+void test_AvrOperatorTable_given_adiw_r27_r26_3(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement Simulator");
+    uint8_t codeMemory[] = {
+		0x1d, 0x96,
+	};
+	uint8_t *progCounter = codeMemory;
+	AvrOperatorTable [*(progCounter + 1)](progCounter);
+	
+	adiw(&codePtr);
+	TEST_ASSERT_EQUAL(codeMemory, codePtr);
+}
+
+/**
+ * Instruction:
+ * 		ADD Rd,Rr
+ *		0000 11rd dddd rrrr
+ * where
+ *		0 <= ddddd <= 31
+ *		0 <= rrrrr <= 31
+ *
+ * Simulate add R4,R7
+ * 		ddddd = 4 = b'00100'
+ * 		rrrrr = 7 = b'00111'
+ *		0000 1100 0100 0111
+ *		  0    c    4    7
+ */
+void test_AvrOperatorTable_given_add_r4_r7(void)
+{
+    uint8_t codeMemory[] = {
+		0x47, 0x0c,
+	};
+	uint8_t *progCounter = codeMemory;
+	AvrOperatorTable [*(progCounter + 1)](progCounter);
 }
