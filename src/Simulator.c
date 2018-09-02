20bdc9eaee2a767f9fdc4550912de0a2daf7d9e5
chaosad <frasker@hotmail.com>
2018-09-02 13:56:54 +0800

    Added dumpRegisters() and test for is2wordInstruction()
    The test currently fails; need to be fixed.
    Also rewrote the pushWord test to improve redability as well as
    correcting the test.

--- src/Simulator.c
+++ src/Simulator.c
@@ -168,6 +168,21 @@ void dumpSram(int startAddr, int byteCount) {
   }
 }
 
+void dumpRegisters(void) {
+  printf("R0 =0x%02x R1 =0x%02x R2 =0x%02x R3 =0x%02x R4 =0x%02x "
+         "R5 =0x%02x R6 =0x%02x R7 =0x%02x R8 =0x%02x R9 =0x%02x \n",
+          r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8], r[9]);
+  printf("R10=0x%02x R11=0x%02x R12=0x%02x R13=0x%02x R14=0x%02x "
+         "R15=0x%02x R16=0x%02x R17=0x%02x R18=0x%02x R19=0x%02x \n",
+          r[10], r[11], r[12], r[13], r[14], r[15], r[16], r[17], r[18], r[19]);
+  printf("R20=0x%02x R21=0x%02x R22=0x%02x R23=0x%02x R24=0x%02x "
+         "R25=0x%02x R26=0x%02x R27=0x%02x R28=0x%02x R29=0x%02x \n",
+          r[20], r[21], r[22], r[23], r[24], r[25], r[26], r[27], r[28], r[29]);
+  printf("R30=0x%02x R31=0x%02x SREG=0x%02x SP=0x%04x "
+         "X=0x%04x Y=0x%04x Z=0x%04x\n",
+         r[30], r[31], *sreg, *(uint16_t *)spl, *xRegPtr, *yRegPtr, *zRegPtr);
+}
+
 int instructionWith1001010(uint8_t *codePtr)
 {
 	uint8_t low4bit;
@@ -257,14 +272,14 @@ int instructionWith1001001(uint8_t *codePtr)
 
 int mulsuORfmulORfmulsORfmulsu(uint8_t *codePtr)
 {
-	uint8_t lowBit;
-	lowBit = ((*codePtr & 0x8) >> 3) | ((*codePtr & 0x80) >> 6);
+	uint8_t lowBits;
+	lowBits = ((*codePtr & 0x8) >> 3) | ((*codePtr & 0x80) >> 6);
 
-	if(lowBit == 0x0)
+	if(lowBits == 0x0)
 		mulsu(codePtr);
-	else if(lowBit == 0x01)
+	else if(lowBits == 0x01)
 		fmul(codePtr);
-	else if(lowBit == 0x2)
+	else if(lowBits == 0x2)
 		fmuls(codePtr);
   else
     fmulsu(codePtr);
